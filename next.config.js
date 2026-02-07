3bccd4a619e8a9fa9ed707bf0b4c39f67f9a664e
Your Name <you@example.com>
2026-02-07 12:17:58 +0600

    Add Unsplash and image CDN domains to Next.js image config

--- next.config.js
+++ next.config.js
@@ -181,6 +181,18 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: '**.supabase.co'
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos'
       }
     ],
     formats: ['image/avif', 'image/webp'],
